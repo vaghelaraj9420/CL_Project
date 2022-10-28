9118952517685f52b517c7a7108bf44306930e3d
vaghelaraj9420 <53364483+vaghelaraj9420@users.noreply.github.com>
2022-10-29 01:25:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import time
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+class PythonHTTP(BaseHTTPRequestHandler):
+
+    def do_GET(self):                               # for controlling get requests 
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # opening the text file
+        with open('index.html','r') as file:
+        
+            while True:
+            
+                # Get next line from file
+                line = file.readline()
+            
+                # if line is empty
+                # end of file is reached
+                if not line:
+                    break
+                print(line)
+                self.wfile.write(bytes(line,"utf-8"))
+
+
+            # for line in file:
+        
+            #     # reading each word        
+            #     for word in line.split():
+        
+            #         # displaying the words           
+            #         # print(word) 
+            #         self.wfile.write(bytes(word,"utf-8"))
+
+        # self.wfile.write(bytes("<html><body><h1>Hello World!</h1></body></html>", "utf-8"))
+        # self.wfile.write(bytes("index.html", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+
+        self.wfile.write(bytes('{"time": "' + date + '"}', "utf-8"))
+
+
+server = HTTPServer((HOST, PORT), PythonHTTP)
+print("Server now running....")
+
+server.serve_forever()
+server.server_close()
+print("Server Stopped!") 
